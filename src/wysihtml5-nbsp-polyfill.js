819f288eaffbcd15aeb770b2bf1d7bd4f4c8a234
Nic Aitch <nic@nicinabox.com>
2014-10-14 10:26:23 -0500

    Strip on change only
    Placing the cursor in the right position on newword is impossible

--- src/wysihtml5-nbsp-polyfill.js
+++ src/wysihtml5-nbsp-polyfill.js
@@ -8,7 +8,6 @@
     var self = Editor.prototype;
 
     self.on('change', stripNbsp);
-    self.on('newword:composer', stripNbsp);
 
     instance = new Editor(editableElement, config);
     return instance;
@@ -17,19 +16,5 @@
   var stripNbsp = _.debounce(function() {
     var val = editor.getValue().replace(/&nbsp;/g, ' ');
     editor.setValue(val, true);
-
-    restoreCursorPosition();
   }, 500);
-
-  var restoreCursorPosition = function() {
-    var area = instance.editableElement;
-    if (document.activeElement === instance.composer.editableArea) {
-      var length = area.value.length;
-
-      console.log(length);
-      area.focus();
-      console.log(instance.composer.editableArea);
-      instance.composer.editableArea.setSelectionRange(length, length);
-    }
-  };
 })();
